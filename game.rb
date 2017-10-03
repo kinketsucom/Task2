829bf35ff6b27f4985ad86e369483098cf0caad7
admin <admin@pc-b139.local>
2017-10-04 02:41:28 +0900

    アルゴリズムは完成したけどまったくオブジェクト指向してない

--- game.rb
+++ game.rb
@@ -1,11 +1,13 @@
 score_a = 0
 score_b = 0
-hit_count_a = 0
-hit_count_b = 0
+hit_meter_a = [0,0,0,0,0,0,0,0,0,0]
+hit_meter_b = [0,0,0,0,0,0,0,0,0,0]
 out = 0
 defense_a = 0
 defense_b = 0
-base = [0,0,0]
+base = 0
+batter_num_a = 1
+batter_num_b = 1
 
 PlayerInfoA = {1 => { attack: 32, defense: 3},
                2 => { attack: 11, defense: 4},
@@ -38,11 +40,182 @@ PlayerInfoB.each do |key,value|
   defense_b += value[:defense]
 end
 
-10.times do
-  if (out<3) then
+for i in 1..5
+    puts("-----")
+    puts(i.to_s+"回表")
+    puts("-----")
+    while(out<3) do #スリーアウトまで繰りかえす
+      hit_meter_a[batter_num_a] += PlayerInfoA[batter_num_a][:attack]
+      if (hit_meter_a[batter_num_a] < defense_b) then  #ヒットにならず
+        out += 1
+      else #ヒット
+        hit_meter_a[batter_num_a] -= defense_b
+        case hit_meter_a[batter_num_a] % 4
+        when 0 then
+          # p "1るいだ"
+          base *= 1000
+          base += 1
+          if(base / 100000 == 1) then
+            score_a += 1
+            base -= 100000
+          end
+          if(base / 10000 == 1) then
+            score_a += 1
+            base -= 10000
+          end
+          if(base / 1000 == 1) then
+            score_a += 1
+            base -= 1000
+          end
+        when 1 then
+          # p "2るいだ"
+          base *= 1000
+          base += 10
+          if(base / 100000 == 1) then
+            score_a += 1
+            base -= 100000
+          end
+          if(base / 10000 == 1) then
+            score_a += 1
+            base -= 10000
+          end
+          if(base / 1000 == 1) then
+            score_a += 1
+            base -= 1000
+          end
 
+        when 2 then
+          # p "3るいだ"
+          base *= 1000
+          base += 100
+          if(base / 100000 == 1) then
+            score_a += 1
+            base -= 100000
+          end
+          if(base / 10000 == 1) then
+            score_a += 1
+            base -= 10000
+          end
+          if(base / 1000 == 1) then
+            score_a += 1
+            base -= 1000
+          end
 
-    
-  end
+        else
+          # p "ホームラン"
+          base *= 1000
+          if(base / 100000 == 1) then
+            score_a += 1
+            base -= 100000
+          end
+          if(base / 10000 == 1) then
+            score_a += 1
+            base -= 10000
+          end
+          if(base / 1000 == 1) then
+            score_a += 1
+            base -= 1000
+          end
+          score_a += 1
+          base = 0
+        end #case
+      end #ifelse
+      if(batter_num_a%9==0) then #周回
+        batter_num_a = 0
+      end
+      batter_num_a += 1
+    end #whileここまで
+    out = 0
+    base = 0
+    puts(score_a.to_s+" - "+score_b.to_s)
+
+# こっから裏
+    puts("-----")
+    puts(i.to_s+"回裏")
+    puts("-----")
+    while(out<3) do #スリーアウトまで繰りかえす
+      hit_meter_b[batter_num_b] += PlayerInfoB[batter_num_b][:attack]
+      if (hit_meter_b[batter_num_b] < defense_a) then  #ヒットにならず
+        out += 1
+      else #ヒット
+        hit_meter_b[batter_num_b] -= defense_a
+        case hit_meter_b[batter_num_b] % 4
+        when 0 then
+          # p "1るいだ"
+          base *= 1000
+          base += 1
+          if(base / 100000 == 1) then
+            score_b += 1
+            base -= 100000
+          end
+          if(base / 10000 == 1) then
+            score_b += 1
+            base -= 10000
+          end
+          if(base / 1000 == 1) then
+            score_b += 1
+            base -= 1000
+          end
+        when 1 then
+          # p "2るいだ"
+          base *= 1000
+          base += 10
+          if(base / 100000 == 1) then
+            score_b += 1
+            base -= 100000
+          end
+          if(base / 10000 == 1) then
+            score_b += 1
+            base -= 10000
+          end
+          if(base / 1000 == 1) then
+            score_b += 1
+            base -= 1000
+          end
+
+        when 2 then
+          # p "3るいだ"
+          base *= 1000
+          base += 100
+          if(base / 100000 == 1) then
+            score_b += 1
+            base -= 100000
+          end
+          if(base / 10000 == 1) then
+            score_b += 1
+            base -= 10000
+          end
+          if(base / 1000 == 1) then
+            score_b += 1
+            base -= 1000
+          end
+
+        else
+          # p "ホームラン"
+          base *= 1000
+          if(base / 100000 == 1) then
+            score_b += 1
+            base -= 100000
+          end
+          if(base / 10000 == 1) then
+            score_b += 1
+            base -= 10000
+          end
+          if(base / 1000 == 1) then
+            score_b += 1
+            base -= 1000
+          end
+          score_b += 1
+          base = 0
+        end
+      end #case
+      if(batter_num_b%9==0) then #周回
+        batter_num_b = 0
+      end
+      batter_num_b += 1
+    end #whileここまで
+    out = 0
+    base = 0
+    puts(score_a.to_s+" - "+score_b.to_s)
 
 end
